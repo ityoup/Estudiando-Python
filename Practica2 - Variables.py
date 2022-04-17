59c4a9d85480ff4b93b226c27536e6c39e6ef3d9
ityoup <lizarraga.juan.1gv@gmail.com>
2022-04-17 10:23:01 -0500

    Se cambio el nombre del archivo, y se hizo explicacion de las variables

---
+++ Practica2 - Variables.py
@@ -0,0 +1,19 @@
+from xml.dom.expatbuilder import Rejecter
+
+
+nombre = "Juan" 
+#Cuando es un numero, no usa comilla
+#Cuando vamos a poner un texto se usa comillas al inicio y al final de cada palabra
+print(nombre)
+#Es necesario poner sangrias
+
+
+# ----- PRACTICA 2 -------------
+# Python le da el tipo de dato a la variable
+nombre, nombre1, nombre2 = "Juan", "Silvia", "ana"
+print(nombre,nombre1,nombre2)
+
+#Para poder asignar variables con el mismo dato se pone asi:
+
+ejemplo = ejemplo1 = ejemplo2 = "Juan"
+print(ejemplo, ejemplo1, ejemplo2)
--- Practica2.py
+++
@@ -1,5 +0,0 @@
-numeros = "Juan" 
-#Cuando es un numero, no usa comilla
-#Cuando vamos a poner un texto se usa comillas al inicio y al final de cada palabra
-print(numeros)
-#S
